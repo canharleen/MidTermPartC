2b3c543b59fa79f3fdcb7249bce68552671d9a82
Harleen Kaur <kau15579@sheridancollege.ca>
2024-02-21 09:58:12 -0500

    Second commit, created local brach Harleen_kaur and added jva class Harleen_Kaur.java to it

---
+++ src/midtermexam_versiona_extensioncode/Harleen_Kaur.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexam_versiona_extensioncode;
+
+/**
+ *
+ * @author gilla
+ */
+public class Harleen_Kaur {
+   //My name is Harleen kaur.
+    //My Student Id is: 991716874
+    //This is my branch named as Harleen_Kaur
+}
